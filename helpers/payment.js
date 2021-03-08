1d8dfad8a8d76bdd1a2da7a48df926e2c23ffe95
chirag003 <55747012+chirag003@users.noreply.github.com>
2021-03-08 11:30:37 +0530

    Update payment.js

--- helpers/payment.js
+++ helpers/payment.js
@@ -18,7 +18,7 @@ function uuidv4() {
 	const options = {
 				  url: 'https://api.test.paysafe.com/paymenthub/v1/payments',
 				  headers: {
-					'Authorization': 'Basic cHJpdmF0ZS03NzUxOkItcWEyLTAtNWYwMzFjZGQtMC0zMDJkMDIxNDQ5NmJlODQ3MzJhMDFmNjkwMjY4ZDNiOGViNzJlNWI4Y2NmOTRlMjIwMjE1MDA4NTkxMzExN2YyZTFhODUzMTUwNWVlOGNjZmM4ZTk4ZGYzY2YxNzQ4',
+					'Authorization': '<private_key>',  //base64
 					'Simulator': 'EXTERNAL',
 					'Content-Type': 'application/json'
 					},
